a3d49e6ee50fc4a62ead18744bb5c771fef9e0db
Ove Gram Nipen <ove@nipen.no>
2023-10-03 11:59:49 +0200

    Init spring project

---
+++ src/test/java/no/kantega/htmxdemo/HtmxdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.kantega.htmxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
